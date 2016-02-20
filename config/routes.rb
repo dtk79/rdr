49c8f09f3deecadefaf587a808531f2dfc4610ec
Dan Knutson <dknutson@gmail.com>
2016-02-20 17:06:45 -0500

    Boostrap no gutters fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
   resources :queens
+
+  root :to => "pages#home"
+
+  Rails.application.routes.draw do
+    get "/pages/:page" => "pages#show"
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
